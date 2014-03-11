484c53e8a687534146f3eb0aef2b1ee60d9150b8
kinmengBGM <kinmeng.chan@beans.com.my>
2014-03-11 09:46:51 +0800

    Fixed relationship issue.

--- leaveapp/employeeprofileservice/src/main/java/com/beans/leaveapp/employee/model/Employee.java
+++ leaveapp/employeeprofileservice/src/main/java/com/beans/leaveapp/employee/model/Employee.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -236,6 +237,7 @@ public class Employee {
 	}
 	
 	@OneToMany(fetch=FetchType.LAZY, targetEntity=Address.class)
+	@JoinColumn(name="employeeid")
 	public List<Address> getAddresses() {
 		return addresses;
 	}
