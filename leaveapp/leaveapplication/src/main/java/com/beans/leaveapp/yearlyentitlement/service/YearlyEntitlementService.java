7df254567536dd4b4fe453a6095a65a6e53b8869
pradeepchinta432@gmail.com <pradeepchinta432@gmail.com>
2014-03-31 18:53:44 +0800

    minor changes updated

--- leaveapp/leaveapplication/src/main/java/com/beans/leaveapp/yearlyentitlement/service/YearlyEntitlementService.java
+++ leaveapp/leaveapplication/src/main/java/com/beans/leaveapp/yearlyentitlement/service/YearlyEntitlementService.java
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
+
 package com.beans.leaveapp.yearlyentitlement.service;
 
 import java.util.List;
 
-import com.beans.leaveapp.employee.model.Employee;
 import com.beans.leaveapp.yearlyentitlement.model.EmployeeEntitlement;
 import com.beans.leaveapp.yearlyentitlement.model.LeaveEntitlement;
 import com.beans.leaveapp.yearlyentitlement.model.YearlyEntitlement;
@@ -33,33 +32,3 @@ public interface YearlyEntitlementService {
 	public List<LeaveEntitlement> findByEmployeeIdAndfindByLeaveTypeId(String x,String y);
 	
 }
-=======
-package com.beans.leaveapp.yearlyentitlement.service;
-
-import java.util.List;
-
-import com.beans.leaveapp.employee.model.Employee;
-import com.beans.leaveapp.yearlyentitlement.model.EmployeeEntitlement;
-import com.beans.leaveapp.yearlyentitlement.model.LeaveEntitlement;
-import com.beans.leaveapp.yearlyentitlement.model.YearlyEntitlement;
-
-public interface YearlyEntitlementService {
-
-	public List<YearlyEntitlement> findAll() throws Exception;
-
-	public YearlyEntitlement update(YearlyEntitlement selectedYearlyEntitlement) throws Exception;
-
-	public YearlyEntitlement delete(int id);
-
-	public YearlyEntitlement create(LeaveEntitlement leaveEntitlement);
-   
-	public List<String> employeeNames();
-	
-	public List<LeaveEntitlement> findLeave();
-
-	public List<String> findLeaveTypes();
-	
-	public List<EmployeeEntitlement> findByEmployeeId(int x);
-	
-}
->>>>>>> branch 'sprint1' of https://github.com/kinmengBGM/bgm-hrm.git
