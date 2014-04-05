1ccaf5678656d1ba25faf3380eba4591478d5862
sengkeat <sengkeat.yeoh@beans.com.my>
2014-04-04 18:34:12 -0700

    Created Welcome Email service and Implementation but still contain bugs such as improper employee details displayed and the pictures that are supposed to appear in the html email did not display as expected.

---
+++ leaveapp/web/src/test/java/com/beans/leaveapp/test/welcomeemail/WelcomeEmailServiceTest.java
@@ -0,0 +1,46 @@
+package com.beans.leaveapp.test.welcomeemail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.beans.common.security.users.model.Users;
+import com.beans.leaveapp.employee.model.Employee;
+import com.beans.leaveapp.welcomeemail.service.WelcomeEmailService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/META-INF/spring-welcomeemail.xml")
+public class WelcomeEmailServiceTest {
+
+	ApplicationContext applicationContext;
+	WelcomeEmailService welcomeEmailService;
+	
+	@Before
+	public void setup() {
+		applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/spring-welcomeemail.xml");
+		welcomeEmailService = (WelcomeEmailService) applicationContext.getBean("welcomeEmailService");
+		
+	}
+	
+	@Test
+	public void testEmailSuccessful() {
+		Employee employee = new Employee();
+		employee.setName("Yeoh Seng Keat");
+		employee.setPosition("Junior Programmer");
+		employee.setPersonalEmailAddress("sengkeat.yeoh@beans.com.my");
+		
+		Users user = new Users();
+		user.setUsername("SengKeat");
+		employee.setUsers(user);
+		
+		
+		
+		welcomeEmailService.sendHTMLEmail(employee);
+	}
+	
+	
+}
