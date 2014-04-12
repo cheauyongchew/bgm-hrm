05efae4fabfe2a11d1bebbae781faab61f38ad5e
lakshminarayanar <rlnarayana4java@gmail.com>
2014-04-13 00:22:07 +0800

    Added Employee Grade validations

--- leaveapp/masterdataservice/src/main/java/com/beans/leaveapp/employeegrade/model/EmployeeGrade.java
+++ leaveapp/masterdataservice/src/main/java/com/beans/leaveapp/employeegrade/model/EmployeeGrade.java
@@ -1,5 +1,7 @@
 package com.beans.leaveapp.employeegrade.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,7 +15,8 @@ import org.hibernate.annotations.Type;
 
 @Entity
 @Table(name="EmployeeGrade")
-public class EmployeeGrade {
+public class EmployeeGrade  implements Serializable{
+	private static final long serialVersionUID = 1L;
 	 private int id;
 	 private String name;
 	 private String description;
