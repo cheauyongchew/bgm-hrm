334bfe5c385f55d4107816aeec6443ef32c754ee
lakshminarayana <lakshminarayana@localhost.localdomain>
2014-06-17 22:51:47 +0800

    Updated Monthly leave report sending mail

--- leaveapp/leaveapplication/src/main/java/com/beans/leaveapp/monthlyreport/SendMonthlyLeaveReportService.java
+++ leaveapp/leaveapplication/src/main/java/com/beans/leaveapp/monthlyreport/SendMonthlyLeaveReportService.java
@@ -3,5 +3,7 @@ package com.beans.leaveapp.monthlyreport;
 public interface SendMonthlyLeaveReportService {
 	
 	void sendMonthlyLeaveReportToEmployees();
+	
+	void sendMonthlyLeaveReportToHR();
 
 }
